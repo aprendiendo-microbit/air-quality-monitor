78d079c825357eba4b3b4ea7a7ebc564799e4bd5
Enrique Crespo <jecrespom@gmail.com>
2024-03-09 21:47:04 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,5 +3,6 @@ basic.forever(function () {
     OLED.clear()
     OLED.writeStringNewLine("Dust(ug/m3)")
     OLED.writeNumNewLine(Environment.ReadDust(DigitalPin.P13, AnalogPin.P1))
+    music.play(music.stringPlayable("C5 C5 A F - - - - ", 120), music.PlaybackMode.UntilDone)
     basic.pause(60000)
 })
